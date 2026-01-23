68658580777dea8808b5b73bb2cacfff66c47bcb
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2026-01-23 09:00:05 -0600

    Tema: Peticiones HTTP y Serialization Laboratorio: Cómo obtener datos de Internet
    - Se agregó el plugin de Kotlinx Serialization en el archivo `build.gradle.kts` del proyecto y se actualizó su configuración en el nivel de aplicación.
    - Se añadió un registro de depuración (`Log.d`) en `MarsViewModel` para visualizar las URLs de las fotos obtenidas en el logcat.
    - Se ajustó la lógica de obtención de datos para iterar sobre la lista de resultados recibida de la API.

--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -15,6 +15,7 @@
  */
 package com.example.marsphotos.ui.screens
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -51,6 +52,9 @@ class MarsViewModel : ViewModel() {
         viewModelScope.launch {
             marsUiState = try {
                 val listResult = MarsApi.retrofitService.getPhotos()
+                listResult.forEach {
+                    Log.d("URL", it.toString())
+                }
                 MarsUiState.Success(
                     "Success: ${listResult.size} Mars photos retrieved"
                 )
