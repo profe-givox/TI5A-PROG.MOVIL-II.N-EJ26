0d4ef3e81d2578fe279d9773b8ebda759b548276
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2026-01-23 08:32:56 -0600

    Tema: Peticiones HTTP y Serialization Laboratorio: Cómo obtener datos de Internet
    - Se actualizó el archivo `MarsApiService.kt` incluyendo la importación de `toMediaType` para la configuración del convertidor de Retrofit.
    - Se realizaron ajustes menores de formato en la clase de datos `MarsPhoto`.

--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -4,6 +4,7 @@ import retrofit2.http.GET
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
+++ basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
@@ -2,6 +2,7 @@ package com.example.marsphotos.network
 
 import kotlinx.serialization.Serializable
 
+
 @Serializable
 data class MarsPhoto(
     val id: String,  val img_src: String
