edb2d730953f5cd93280edd3000848d11c4c7e2e
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2026-02-05 08:57:58 -0600

    Actualizamos el worker, creamos la forma de obtener la uri de la imagen y procesarla para obtener datos de salida usando OuputData.

--- basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
+++ basic-android-kotlin-compose-training-workmanager-starter/app/src/main/java/com/example/bluromatic/workers/BlurWorker.kt
@@ -4,18 +4,23 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
+import androidx.work.workDataOf
 import com.example.bluromatic.DELAY_TIME_MILLIS
 import com.example.bluromatic.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
+import com.example.bluromatic.KEY_BLUR_LEVEL
+import com.example.bluromatic.KEY_IMAGE_URI
 
 private const val TAG = "BlurWorker"
 
 class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, params) {
     override suspend fun doWork(): Result {
-
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        val blurLevel = inputData.getInt(KEY_BLUR_LEVEL, 1)
         makeStatusNotification(
             applicationContext.resources.getString(R.string.blurring_image),
             applicationContext
@@ -24,20 +29,32 @@ class BlurWorker(ctx: Context, params: WorkerParameters) : CoroutineWorker(ctx, 
         return withContext(Dispatchers.IO) {
             delay(DELAY_TIME_MILLIS)
             return@withContext try {
+                require(!resourceUri.isNullOrBlank()) {
+                    val errorMessage =
+                        applicationContext.resources.getString(R.string.invalid_input_uri)
+                    Log.e(TAG, errorMessage)
+                    errorMessage
+                }
+               /* val resolver = applicationContext.contentResolver
                 val picture = BitmapFactory.decodeResource(
                     applicationContext.resources,
                     R.drawable.android_cupcake
+                )*/
+                val resolver = applicationContext.contentResolver
+
+                val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri))
                 )
-                val output = blurBitmap(picture, 1)
+                val output = blurBitmap(picture, blurLevel)
 
                 val outputUri = writeBitmapToFile(applicationContext, output)
 
-                makeStatusNotification(
+                /*makeStatusNotification(
                     "Output is $outputUri",
                     applicationContext
-                )
-
-                Result.success()
+                )*/
+                val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+                Result.success(outputData)
             } catch (throwable: Throwable) {
                 Log.e(
                     TAG,
